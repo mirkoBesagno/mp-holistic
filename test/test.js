16e4e005ef154a25ce48845e7d3f148a704a6529
mirko <50323497+mirkoBesagno@users.noreply.github.com>
2022-04-29 13:08:28 +0200

    WIP

--- test/test.js
+++ test/test.js
@@ -5,6 +5,7 @@ describe('test-and', function () {
   it('-1', function (done) {
     try {
         console.log('terst 1');
+        done();
     } catch (error) {
       console.log(error);
       done(new Error("Errore!"));
